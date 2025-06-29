5714bad6950ee4f438d5d0286aed2bf5e154baaa
Jørgen Andersen <jorg1piano@gmail.com>
2025-06-29 14:53:49 +0200

    Add util to create a random hex color

--- util/random.go
+++ util/random.go
@@ -13,3 +13,12 @@ func RandomId() string {
 	}
 	return hex.EncodeToString(bytes)
 }
+
+func RandomColor() string {
+	bytes := make([]byte, 3)
+	_, err := rand.Read(bytes)
+	if err != nil {
+		return "#000000"
+	}
+	return "#" + hex.EncodeToString(bytes)
+}
